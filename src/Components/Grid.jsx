47b9a7ae0cab714e6f4ef9da1b3adaa031a2420b
Isra O <Isra@israadam1993.com>
2020-07-10 23:13:11 -0400

    news rendered

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -1,34 +1,71 @@
 import React from "react";
 import NewCard from "./NewCard";
 
-class Grid extends React.Component {
-  render() {
-    return (
-      <div className="grid">
-        <div className="grid_item">
-          <NewCard />
-        </div>
-        <div className="grid_item">
-          <NewCard />
-        </div>
-        <div className="grid_item">
-          <NewCard />
-        </div>
-        <div className="grid_item">
-          <NewCard />
-        </div>
-        <div className="grid_item">
-          <NewCard />
-        </div>
-        <div className="grid_item">
-          <NewCard />
-        </div>
-        <div className="grid_item">
-          <NewCard />
-        </div>
-      </div>
-    );
-  }
+function Grid() {
+  const news = [
+    {
+      id: 1,
+      img: "",
+      category: "Health",
+      title: "Experts Confident Biden’s COVID-19 Response Could Speed Recovery",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque sit amet porttitor eget. Eget egestas purus viverra accumsan. Commodo odio aenean sed adipiscing diam donec",
+    },
+
+    {
+      id: 2,
+      img: "",
+      category: "Technology",
+      title:
+        "Coronavirus: Why Singapore turned to wearable contact-tracing tech",
+      text:
+        "The wearable devices complement the island's existing contact-tracing app, to identify people who might have been infected by those who have tested positive for the virus. All users have to do is carry one, and the battery lasts up to nine months without needing a recharge - something one expert said had stunned him.",
+    },
+
+    {
+      id: 3,
+      img: "",
+      category: "Politics",
+      title:
+        "Justice Kavanaugh denies emergency request from Illinois GOP groups seeking to hold large rallies",
+      text:
+        "The Illinois Republican Party is continuing to push the lawsuit forward in the lower courts, but the Supreme Court will not be stepping in to permit political gatherings.",
+    },
+
+    {
+      id: 4,
+      img: "",
+      category: "Politics",
+      title:
+        "Justice Kavanaugh denies emergency request from Illinois GOP groups seeking to hold large rallies",
+      text:
+        "The Illinois Republican Party is continuing to push the lawsuit forward in the lower courts, but the Supreme Court will not be stepping in to permit political gatherings.",
+    },
+
+    {
+      id: 5,
+      img: "",
+      category: "Politics",
+      title:
+        "Justice Kavanaugh denies emergency request from Illinois GOP groups seeking to hold large rallies",
+      text:
+        "The Illinois Republican Party is continuing to push the lawsuit forward in the lower courts, but the Supreme Court will not be stepping in to permit political gatherings.",
+    },
+  ];
+
+  const newsList = news.map((news) => (
+    <div className="grid_item">
+      {" "}
+      <NewCard
+        key={news.id}
+        category={news.category}
+        title={news.title}
+        text={news.text}
+      />
+    </div>
+  ));
+
+  return <div className="grid">{newsList}</div>;
 }
 
 export default Grid;
--- src/Components/NewCard.jsx
+++ src/Components/NewCard.jsx
@@ -1,24 +1,20 @@
 import React from "react";
-import Enter1 from "../imgs/entertainment.jpg";
+import Enter1 from "../imgs/politics.jpg";
 
-class NewCard extends React.Component {
-  render() {
-    return (
-      <div className="news-card">
-        <img className="news-card_img" src={Enter1} alt="img" />
-        <div className="news-card_content">
-          <div className="catogray">Food</div>
-          <h3 className="title">COVID19</h3>
-          <span className="date">Published: </span>
-          <p className="text">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
-            quam ullam et fugiat perspiciatis veritatis deleniti doloribus
-            repellat magnam accusamus.
-          </p>
-        </div>
+function NewCard(props) {
+  return (
+    <div className="news-card">
+      <div className="news-card_img">
+        <img src={Enter1} alt="img" />
       </div>
-    );
-  }
+      <div className="news-card_content">
+        <div className="category">{props.category}</div>
+        <h3 className="title">{props.title}</h3>
+        <span className="date">Published: </span>
+        <p className="text">{props.text}</p>
+      </div>
+    </div>
+  );
 }
 
 export default NewCard;
